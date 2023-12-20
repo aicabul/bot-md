4addbe2098c9170afe6b8cc32c8d8b5b87f7a11c
inrl-official <fasweehmon2@gmail.com>
2023-12-20 21:26:11 +0530

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -1,24 +1,23 @@
 const {
     inrl,
-    extractUrlsFromString
+    extractUrlsFromString,
+    config,
+    getJson,
+    getBuffer,
 } = require('../lib/');
-const axios = require('axios');
-const {
-    BASE_URL
-} = require('../config');
 
 inrl({
     pattern: 'fb ? (.*)',
-    desc: 'download medias frok Facebook',
+    desc: 'download medias from Facebook',
     react: "⬇️",
     type: "downloader"
 }, async (message, match) => {
-match = match || message.quoted.text;
+match = match || message.reply_message.text;
     if (!match)  return await message.reply("*_give me a url_*");
     const urls = extractUrlsFromString(match);
     if(!urls[0]) return await message.send("*_Give me a valid url_*");
-        let {data} = await axios(`${BASE_URL}api/fb?url=${urls[0]}`);
-        const {status, description, url } = data;
+        const data = await getJson(`${config.BASE_URL}api/fb?url=${urls[0]}`);
+        const {status, result } = data;
         if(!status) return await message.send("*Not Found*");
-        return await message.sendReply(url, {caption:description}, "video");
+        return await message.send(await getBuffer(result.hd), {}, 'video');
 });
